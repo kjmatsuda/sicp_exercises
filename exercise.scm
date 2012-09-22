50f891065ca7f6eb4a26db13673f102fddb6bc10
Koji Matsuda <kjmatsuda1985@gmail.com>
2012-09-22 16:10:45 +0900

    Ex 1.19 の回答。実行結果がおかしい(fib 5) => 6

--- exercise.scm
+++ exercise.scm
@@ -32,3 +32,22 @@
       (+ (recursive-calc (- x 1))
 	 (* 2 (recursive-calc (- x 2)))
 	 (* 3 (recursive-calc (- x 3))))))
+
+;; Ex 1.20
+(define (fib n)
+  (fib-iter 1 0 0 1 n))
+
+(define (fib-iter a b p q count)
+  (cond ((= count 0) b)
+	((even? count)
+	 (fib-iter a
+		   b
+		   (+ (* p p) (* q q))   ; compute p'
+		   (+ (* 2 p q) (* q q)) ; compute q'
+		   (/ count 2)))
+	(else (fib-iter (+ (* b q) (* a q) (* a p))
+			 (+ (* b q) (* a q))
+			 p
+			 q
+			 (- count 1)))))
+	 
