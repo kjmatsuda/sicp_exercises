97cfc6ca4f73feeb7a1791a7948c867df3b10528
Koji Matsuda <kjmatsuda1985@gmail.com>
2012-09-24 21:26:42 +0900

    Ex 1.30. Add my answer, which is wrong with its start condition.

--- exercise.scm
+++ exercise.scm
@@ -129,3 +129,11 @@
   (if (= n 2)
       3
       (+ n 2)))
+
+;; Ex 1.30
+(define (sum term a next b)
+  (define (iter a result)
+    (if (> a b)
+	result
+	(iter (next a) (+ result (term a)))))
+  (iter a (term a))) ;; 間違い。正しくは (iter a 0)
