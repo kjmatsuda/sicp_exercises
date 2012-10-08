0be2d0bfbe7255d9f72344c533f035a029dd6cea
Koji Matsuda <kjmatsuda1985@gmail.com>
2012-10-08 21:12:07 +0900

    add answer for Ex 2.2

--- exercise.scm
+++ exercise.scm
@@ -262,3 +262,43 @@
 	  (and (> n 0) (< d 0)))
       -1
       1))
+
+;; Ex 2.2
+;; definition of line segment
+(define (make-segment start end)
+  (cons start end))
+
+(define (start-segment segment)
+  (car segment))
+
+(define (end-segment segment)
+  (cdr segment))
+
+;; definition of point
+(define (make-point x y)
+  (cons x y))
+
+(define (x-point point)
+  (car point))
+
+(define (y-point point)
+  (cdr point))
+
+;; returns midpoint of line segment
+(define (midpoint-segment segment)
+  (cons (average (x-point (start-segment segment))
+		 (x-point (end-segment segment)))
+	(average (y-point (start-segment segment))
+		 (y-point (end-segment segment)))))
+
+
+(define (average a b)
+  (/ (+ a b) 2.0))
+
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
