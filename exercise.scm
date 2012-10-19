d643b4651b97b8481a091cbf2a3c9fecea1dd199
Koji Matsuda <kjmatsuda1985@gmail.com>
2012-10-19 22:41:25 +0900

    Add answer for Ex 2.30.

--- exercise.scm
+++ exercise.scm
@@ -416,3 +416,25 @@
 	  ;; ここでリストにしてるから常にペアが成立する
 	  (fringe (list (car items))) 
 	  (fringe (cdr items))))))
+
+;; Ex 2.30
+(define (square-tree tree)
+  (cond ((null? tree) nil)
+	((not (pair? tree))
+	 (* tree tree))
+	(else (cons (square-tree (car tree))
+		      (square-tree (cdr tree))))))
+;; テストデータ
+;; (square-tree 
+;;  (list 1
+;;        (list 2 (list 3 4) 5)
+;;        (list 6 7)))
+;; => (1 (4 (9 16) 25) (36 49))
+
+;; map版
+(define (square-tree tree)
+  (map (lambda (sub-tree)
+	 (if (pair? sub-tree)
+	     (square-tree sub-tree)
+	     (* sub-tree sub-tree)))
+       tree))
