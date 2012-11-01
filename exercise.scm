0b2180c63cc1787a25a36e3d24daf9183fa93883
Koji Matsuda <kjmatsuda1985@gmail.com>
2012-11-01 23:21:27 +0900

    Add answer for Ex 2.51.

--- exercise.scm
+++ exercise.scm
@@ -629,3 +629,24 @@
 		     (make-vect 0.0 1.0)
 		     (make-vect 0.0 0.0)
 		     (make-vect 1.0 1.0)))
+
+;; Ex 2.51
+(define (below painter1 painter2)
+  (let ((split-point (make-vect 0.0 0.5)))
+    (let ((point-bottom
+	   (transform-painter painter1
+			      (make-vect 0.0 0.0)
+			      (make-vect 1.0 0.0)
+			      split-point))
+	  (point-top
+	   (transform-painter painter2
+			      split-point
+			      (make-vect 1.0 0.5)
+			      (make-vect 0.0 1.0))))
+      (lambda (frame)
+	(point-bottom frame)
+	(point-top frame)))))
+
+(define (below painter1 painter2)
+  (rotate90 (beside (rotate270 painter1)
+		    (rotate270 painter2))))
