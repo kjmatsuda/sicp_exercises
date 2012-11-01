6b070e1d1e2012d83ea1efdcb0fae8b58899e51a
Koji Matsuda <kjmatsuda1985@gmail.com>
2012-11-01 23:00:16 +0900

    Add answer for Ex 2.50.

--- exercise.scm
+++ exercise.scm
@@ -611,3 +611,21 @@
 				    (make-vect 0.5 0))
 		      (make-segment (make-vect 0.5 0)
 				    (make-vect 0 0.5)))))
+;; Ex 2.50
+(define (flip-horiz painter)
+  (transform-painter painter
+		     (make-vect 1.0 0.0)
+		     (make-vect 0.0 0.0)
+		     (make-vect 1.0 1.0)))
+
+(define (rotate180 painter)
+  (transform-painter painter
+		     (make-vect 1.0 1.0)
+		     (make-vect 0.0 1.0)
+		     (make-vect 1.0 0.0)))
+
+(define (rotate270 painter)
+  (transform-painter painter
+		     (make-vect 0.0 1.0)
+		     (make-vect 0.0 0.0)
+		     (make-vect 1.0 1.0)))
