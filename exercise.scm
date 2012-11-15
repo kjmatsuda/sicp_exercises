b1c71d27a573ce42ce64b15216d3efd1acd28af3
Koji Matsuda <kjmatsuda1985@gmail.com>
2012-11-15 22:01:41 +0900

    Add answer for Ex 2.59.

--- exercise.scm
+++ exercise.scm
@@ -701,3 +701,18 @@
         (else
          (error "unknown expression type -- DERIV" exp))))
 
+;; Ex 2.59
+(define (element-of-set? x set)
+  (cond ((null? set) #f)
+	((equal? x (car set)) #t)
+	(else (element-of-set? x (cdr set)))))
+
+(define (union-set set1 set2)
+  (cond ((and (null? set1) (null? set2)) '())
+	((null? set1) set2)
+	((null? set2) set1)
+	((not (element-of-set? (car set1) set2))
+	 (cons (car set1)
+	       (union-set (cdr set1) set2)))
+	(else
+	 (union-set (cdr set1) set2))))
