76c2fcdb456a264cc001926a71804415c66d9ddb
Koji Matsuda <kjmatsuda1985@gmail.com>
2012-11-24 13:47:36 +0900

    Add code on text for Ex 2.63.

--- exercise.scm
+++ exercise.scm
@@ -741,3 +741,35 @@
 		  (cons x1 (union-set (cdr set1) set2)))
 		 ((> x1 x2)
 		  (cons x2 (union-set set1 (cdr set2)))))))))
+
+;; Ex 2.63
+;; 木構造を定義
+(define (entry tree)
+  (car tree))
+
+(define (left-branch tree)
+  (cadr tree))
+
+(define (right-branch tree)
+  (caddr tree))
+
+(define (make-tree entry left right)
+  (list entry left right))
+
+(define (tree->list-1 tree)
+  (if (null? tree)
+      '()
+      (append (tree->list-1 (left-branch tree))
+	      (cons (entry tree)
+		    (tree->list-1 (right-branch tree))))))
+
+(define (tree->list-2 tree)
+  (define (copy-to-list tree result-list)
+    (if (null? tree)
+	result-list
+	(copy-to-list (left-branch tree)
+		      (cons (entry tree)
+			    (copy-to-list (right-branch tree)
+					  result-list)))))
+  (copy-to-list tree '()))
+
