6118b02fbd2d00a5c864455ecdc82b401a71a7b0
Koji Matsuda <kjmatsuda1985@gmail.com>
2012-11-24 13:27:05 +0900

    Add answer for Ex 2.61 and Ex 2.62.

--- exercise.scm
+++ exercise.scm
@@ -716,3 +716,28 @@
 	       (union-set (cdr set1) set2)))
 	(else
 	 (union-set (cdr set1) set2))))
+
+;; Ex 2.61
+(define (adjoin-set x set)
+  (cond ((null? set)
+	 (cons x '()))
+	((= x (car set))
+	 set)
+	((< x (car set))
+	 (cons x set))
+	(else
+	 (cons (car set) (adjoin-set x (cdr set))))))
+
+;; Ex 2.62
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+	((null? set2) set1)
+	(else
+	 (let ((x1 (car set1))
+	       (x2 (car set2)))
+	   (cond ((= x1 x2)
+		  (cons x1 (union-set (cdr set1) (cdr set2))))
+		 ((< x1 x2)
+		  (cons x1 (union-set (cdr set1) set2)))
+		 ((> x1 x2)
+		  (cons x2 (union-set set1 (cdr set2)))))))))
