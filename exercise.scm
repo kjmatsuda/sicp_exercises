5106f80f145b1e0e0c380bef7bf32db197435f0e
Koji Matsuda <kjmatsuda1985@gmail.com>
2012-12-01 09:45:18 +0900

    Fix answer for Ex.2.69. 末尾再帰で書き直した。以下のようにすると実行結果が見やすい (display (generate-haffman-tree sample-pairs))

--- exercise.scm
+++ exercise.scm
@@ -834,6 +834,19 @@
 	((= bit 1) (right-branch branch))
 	(else (error "bad bit -- CHOOSE-BRANCH" bit))))
 
+(define (adjoin-set x set)
+  (cond ((null? set) (list x))
+	((< (weight x) (weight (car set))) (cons x set))
+	(else (cons (car set)
+		    (adjoin-set x (cdr set))))))
+
+(define (make-leaf-set pairs)
+  (if (null? pairs)
+      '()
+      (let ((pair (car pairs)))
+	(adjoin-set (make-leaf (car pair)   ; symbol
+			       (cadr pair))  ; frequency
+		    (make-leaf-set (cdr pairs))))))
 ;; Ex 2.67
 (define sample-tree
   (make-code-tree (make-leaf 'A 4)
@@ -870,11 +883,14 @@
   (successive-merge (make-leaf-set pairs)))
 
 (define (successive-merge set)
-  (cond ((null? set) '())
-	((null? (cdr set)) set)
-	(else
-	 (adjoin-set (make-code-tree (car set) (cadr set))
-		     ; これではマージする順がおかしくなりそう
-		     (successive-merge (cddr set))))))
+  (define (successive-merge-1 set result)
+    (cond ((null? set) '())
+	  ((null? (cdr set)) result)
+	  (else
+	   (successive-merge-1 (adjoin-set (make-code-tree (car set) (cadr set))
+				(cddr set))
+			       (adjoin-set (make-code-tree (car set) (cadr set))
+				result)))))
+  (successive-merge-1 set '()))
 
 (define sample-pairs '((A 4) (B 2) (C 1) (D 1)))
