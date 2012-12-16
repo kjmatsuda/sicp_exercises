9c6b4782ed30c8ad975548f7672aa3d5adce8b25
Koji Matsuda <kjmatsuda1985@gmail.com>
2012-12-16 11:49:59 +0900

    Add sample code of Ch 2.4. 複素数を直交座標、極座標それぞれの形で表現する。 それを共存させるためにタグを用いる方法と 表を用いる方法を示す。

--- exercise.scm
+++ exercise.scm
@@ -909,3 +909,236 @@
 (encode 
  rock-lyrics
  (generate-haffman-tree symbols-of-rock-songs))
+
+;; 2.4.1 Representations for Complex Numbers
+;; テキストから引用
+(define (add-complex z1 z2)
+  (make-from-real-imag (+ (real-part z1) (real-part z2))
+		       (+ (imag-part z1) (imag-part z2))))
+
+(define (sub-complex z1 z2)
+  (make-from-real-imag (- (real-part z1) (real-part z2))
+		       (- (imag-part z1) (imag-part z2))))
+
+(define (mul-complex z1 z2)
+  (make-from-mag-ang (* (magnitude z1) (magnitude z2))
+		     (+ (angle z1) (angle z2))))
+
+(define (div-complex z1 z2)
+  (make-from-mag-ang (/ (magnitude z1) (magnitude z2))
+		     (- (angle z1) (angle z2))))
+
+;; Ben の方法
+;; 複素数を直交座標系で表現
+(define (real-part z) (car z))
+
+(define (imag-part z) (cdr z))
+
+(define (magnitude z)
+  (sqrt (+ (square (real-part z))
+	   (square (imag-part z)))))
+
+(define (angle z)
+  (atan (imag-part z) (real-part z)))
+
+(define (make-from-real-imag x y) (cons x y))
+
+(define (make-from-mag-ang r a)
+  (cons (* r (cos a)) (* r (sin a))))
+
+(define z1 (make-from-real-imag 3 4))
+(define z2 (make-from-real-imag 2 9))
+
+;; Alyssa の方法
+;; 複素数を極座標系で表現
+(define (real-part z)
+  (* (magnitude z) (cos (angle z))))
+
+(define (imag-part z)
+  (* (magnitude z) (sin (angle z))))
+
+(define (magnitude z) (car z))
+
+(define (angle z) (cdr z))
+
+(define (make-from-real-imag x y)
+  (cons (sqrt (+ (square x) (square y)))
+	(atan y x)))
+
+(define (make-from-mag-ang r a)
+  (cons r a))
+
+;; Ben, Alyssa の方法を共存させるための方法
+;; 方法1：タグを用いて分岐させる
+(define (attach-tag type-tag contents)
+  (cons type-tag contents))
+
+(define (type-tag datum)
+  (if (pair? datum)
+      (car datum)
+      (error "Bad tagged datum -- TYPE-TAG" datum)))
+
+(define (contents datum)
+  (if (pair? datum)
+      (cdr datum)
+      (error "Bad tagged datum -- CONTENTS" datum)))
+
+(define (rectangular? z)
+  (eq? (type-tag z) 'rectangular))
+
+(define (polar? z)
+  (eq? (type-tag z) 'polar))
+
+;; Ben の方法(直交座標)
+(define (real-part-rectangular z) (car z))
+
+(define (imag-part-rectangular z) (cdr z))
+
+(define (magnitude-rectangular z)
+  (sqrt (+ (square (real-part-rectangular z))
+	   (square (imag-part-rectangular z)))))
+
+(define (angle-rectangular z)
+  (atan (imag-part-rectangular z) (real-part-rectangular z)))
+
+(define (make-from-real-imag-rectangular x y) 
+  (attach-tag 'rectangular (cons x y)))
+
+(define (make-from-mag-ang-rectangular r a)
+  (attach-tag 'rectangular 
+	      (cons (* r (cos a)) (* r (sin a)))))
+
+(define z1 (make-from-real-imag-rectangular 5 8))
+(define z2 (make-from-real-imag-rectangular 7 4))
+
+;; Alyssa の方法(極座標)
+(define (real-part-polar z)
+  (* (magnitude z) (cos (angle z))))
+
+(define (imag-part-polar z)
+  (* (magnitude z) (sin (angle z))))
+
+(define (magnitude-polar z) (car z))
+
+(define (angle-polar z) (cdr z))
+
+(define (make-from-real-imag-polar x y)
+  (attach-tag 'polar
+	      (cons (sqrt (+ (square x) (square y)))
+		    (atan y x))))
+
+(define (make-from-mag-ang-polar r a)
+  (attach-tag 'polar
+	      (cons r a)))
+
+;; generic selector
+(define (real-part z)
+  (cond ((rectangular? z)
+	 (real-part-rectangular (contents z)))
+	((polar? z)
+	 (real-part-polar (contents z)))
+	(else (error "Unknown type -- REAL-PART" z))))
+
+(define (imag-part z)
+  (cond ((rectangular? z)
+	 (imag-part-rectangular (contents z)))
+	((polar? z)
+	 (imag-part-polar (contents z)))
+	(else (error "Unknown type -- IMAG-PART" z))))
+
+(define (magnitude z)
+  (cond ((rectangular? z)
+	 (magnitude-rectangular (contents z)))
+	((polar? z)
+	 (magnitude-polar (contents z)))
+	(else (error "Unknown type -- MAGNITUDE" z))))
+
+(define (angle z)
+  (cond ((rectangular? z)
+	 (angle-rectangular (contents z)))
+	((polar? z)
+	 (angle-polar (contents z)))
+	(else (error "Unknown type -- ANGLE" z))))
+
+;; constructor
+(define (make-from-real-imag x y)
+  (make-from-real-imag-rectangular x y))
+
+(define (make-from-mag-ang r a)
+  (make-from-mag-ang-polar r a))
+
+;; Ben, Alyssa の方法を共存させるための方法
+;; 方法2：表を使った方法
+
+;; Ben の方法(直交座標)
+(define (install-rectangular-package)
+  ;; internal procedures
+  (define (real-part z) (car z))
+  (define (imag-part z) (cdr z))
+  (define (magnitude z)
+    (sqrt (+ (square (real-part z))
+	     (square (imag-part z)))))
+  (define (angle z)
+    (atan (imag-part z) (real-part z)))
+  (define (make-from-real-imag x y) (cons x y))
+  (define (make-from-mag-ang r a)
+    (cons (* r (cos a)) (* r (sin a))))
+
+  ;; interface to the rest of the system
+  (define (tag x) (attach-tag 'rectangular x))
+  (put 'real-part '(rectangular) real-part)
+  (put 'imag-part '(rectangular) imag-part)
+  (put 'magnitude '(rectangular) magnitude)
+  (put 'angle '(rectangular) angle)
+  (put 'make-from-real-imag 'rectangular
+       (lambda (x y) (tag (make-from-real-imag x y))))
+  (put 'make-from-mag-ang 'rectangular
+       (lambda (r a) (tag (make-from-mag-ang r a))))
+  'done)
+
+;; Alyssa の方法(極座標)
+(define (install-polar-package)
+  ;; internal procedures
+  (define (real-part z)
+    (* (magnitude z) (cos (angle z))))
+  (define (imag-part z)
+    (* (magnitude z) (sin (angle z))))
+  (define (magnitude z) (car z))
+  (define (angle z) (cdr z))
+  (define (make-from-real-imag x y)
+    (cons (sqrt (+ (square x) (square y)))
+	  (atan y x)))
+  (define (make-from-mag-ang r a)
+    (cons r a))
+
+  ;; interface to the rest of the system
+  (define (tag x) (attach-tag 'polar x))
+  (put 'real-part '(polar) real-part)
+  (put 'imag-part '(polar) imag-part)
+  (put 'magnitude '(polar) magnitude)
+  (put 'angle '(polar) angle)
+  (put 'make-from-real-imag 'polar
+       (lambda (x y) (tag (make-from-real-imag x y))))
+  (put 'make-from-mag-ang 'polar
+       (lambda (r a) (tag (make-from-mag-ang r a))))
+  'done)
+
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+	  (apply proc (map contents args))
+	  (error 
+	   "No method for these types -- APPLY-GENERIC"
+	   (list op type-tags))))))
+
+(define (real-part z) (apply-generic 'real-part z))
+(define (imag-part z) (apply-generic 'imag-part z))
+(define (magnitude z) (apply-generic 'magnitude z))
+(define (angle z) (apply-generic 'angle z))
+
+(define (make-from-real-imag x y)
+  ((get 'make-from-real-imag 'rectangular) x y))
+
+(define (make-from-mag-ang r a)
+  ((get 'make-from-mag-ang 'polar) r a))
