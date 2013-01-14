a3a82ad5813b11a2d216036017025f20679d134e
Koji Matsuda <kjmatsuda1985@gmail.com>
2013-01-14 21:30:36 +0900

    算術関数コアの関数にタグのついていない数値が来たときに エラーにならないよう修正(Ex 2.78)

--- arithmetic-core.scm
+++ arithmetic-core.scm
@@ -2,14 +2,14 @@
   (cons type-tag contents))
 
 (define (type-tag datum)
-  (if (pair? datum)
-      (car datum)
-      (error "Bad tagged datum -- TYPE-TAG" datum)))
+  (cond ((number? datum) 'scheme-number)
+        ((pair? datum) (car datum))
+        (else "Bad tagged datum -- TYPE-TAG" datum)))
 
 (define (contents datum)
-  (if (pair? datum)
-      (cdr datum)
-      (error "Bad tagged datum -- CONTENTS" datum)))
+  (cond ((number? datum) datum)
+        ((pair? datum) (cdr datum))
+        (else "Bad tagged datum - CONTENTS" datum)))
 
 (define (rectangular? z)
   (eq? (type-tag z) 'rectangular))
