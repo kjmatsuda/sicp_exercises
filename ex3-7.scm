d27b67e49781ca56fc10b7156501c34424c3fec8
Koji Matsuda <kjmatsuda1985@gmail.com>
2013-01-27 11:05:35 +0900

    Ex 3.7 の解答、修正途中。id とパスワードによる照合の方針が行き詰まる。

--- ex3-7.scm
+++ ex3-7.scm
@@ -4,6 +4,10 @@
   ((acc password 'add-password) new-password) acc)
   
 (define (make-account balance secret-password)
+  ;; id と パスワードを合わせて照合を行う
+  (define id 0)
+  (define id-and-password
+    (cons id secret-password))
   (define (withdraw amount)
     (if (>= balance amount)
 	(begin (set! balance (- balance amount))
@@ -15,15 +19,26 @@
 
   ;; 返り値はパスワードが追加されたというメッセージ
   (define (add-password new-password)
-    (set! secret-password (cons new-password secret-password))
+    (set! id-and-password (cons (cons (+ 1 id) new-password) id-and-password))
     (format "Add password: ~a" new-password))
-  
+
+  (define (id-and-password-match? id-pass acc-id-pass-list)
+    (if (or (not (pair? acc-id-pass-list)) (null? acc-id-pass-list))
+	#f
+	(let ((acc-id-pass (car acc-id-pass-list)))
+	  (if (and (eq? (car id-pass) (car acc-id-pass))
+		   (eq? (cdr id-pass) (cdr acc-id-pass)))
+	      #t
+	      (id-and-password-match? id-pass (cdr acc-id-pass-list))))))
+
   (define (error-message amount)
     ;; 引数に金額を受け取るが無視
     "Incorrect Password")
   (define (dispatch password fn)
     ;; パスワード照合の変更
-    (cond ((or (null? password) (not (memq password secret-password)))
+    ;; う~ん、ここで照合のためにアクセス者の id が分かってないといけないが分からない
+    ;; 行き詰まった
+    (cond ((or (null? password) (not (id-and-password-match? password secret-password)))
 	   error-message)
 	  ((eq? fn 'withdraw) withdraw)
 	  ((eq? fn 'deposit) deposit)
