259690cda70455cff8f76c86dfa464febcc907a4
Koji Matsuda <kjmatsuda1985@gmail.com>
2013-02-11 18:41:24 +0900

    Add file for Ex 3.17

---
+++ ex3-17.scm
@@ -0,0 +1,14 @@
+(define (count-pairs x)
+  (if (not (pair? x))
+      0
+      (+ (count-pairs (car x))
+	 (count-pairs (cdr x))
+	 1)))
+
+(define x (list 'a 'b))
+(define z1 (cons x x))
+(define z2 (cons (list 'a 'b) (list 'a 'b)))
+
+(use slib)
+(require 'trace)
+(trace count-pairs)
