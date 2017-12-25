cc5a56a78af8a5721ee202765614ef6fdbfc7253
Koji Matsuda <kjmatsuda1985@gmail.com>
2017-12-25 23:22:44 +0900

    copy and paste correct answer for Ex3.54 from reference web site.

--- ex3-54.scm
+++ ex3-54.scm
@@ -1,4 +1,20 @@
 (define (mul-sreams s1 s2)
   (stream-map * s1 s2))
 
-(define factorials (cons-stream 1 (mul-sreams factorials integers)))
+(define factorials
+  (cons-stream 1
+               (mul-sreams factorials integers)))
+
+;; 正解は下記
+;; 参考サイトからコピペ
+(define factorials
+  (cons-stream 1
+               (mul-sreams
+                factorials
+                (add-streams ones integers))))
+
+(define factorials
+  (cons-stream 1
+               (mul-sreams
+                factorials
+                (stream-cdr integers))))
