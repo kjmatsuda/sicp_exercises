189c2f7b6d3281c8a0d96621b2063ead7f941236
Koji Matsuda <kjmatsuda1985@gmail.com>
2017-12-26 22:16:14 +0900

    Add answer for Ex3.56.

---
+++ ex3-56.scm
@@ -0,0 +1,8 @@
+(define S
+  (cons-stream 1
+               (merge 
+                (merge
+                 (scale-stream S 2)
+                 (scale-stream S 3))
+                (scale-stream S 5)
+                )))
